3ec2034630e2b39e7cda51ac86e0ae1171d471c9
Richard-TR <46071140+Richard-TR@users.noreply.github.com>
2021-07-22 19:29:07 +0100

    Create wcfm_vendor_export.sql

---
+++ wcfm_vendor_export.sql
@@ -0,0 +1,12 @@
+/*
+Run against your database to retrieve list of vendors currently on your store.
+You can modify this to include any information about the vendor you would like.
+You can also filter this further, for example by using the meta_key '_wcfm_store_offline' 
+to return only online stores.
+*/
+
+SELECT ID, display_name, user_login, user_email
+FROM (SELECT * FROM `DB_NAME_HERE`.`PREFIX_usermeta`) T1 
+INNER JOIN (SELECT * FROM `DB_NAME_HERE`.`PREFIX_users`) T2 ON T1.`user_ID` = T2.`ID`
+WHERE meta_key LIKE '%wcfm_vendor%'
+GROUP BY user_ID
